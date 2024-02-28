18997aec4d5c1ea2709c6a6a6055eecba33e72ec
alex_ukrainskyy <ukrainskyy@gmail.com>
2024-02-28 11:28:20 +0300

    first commit

---
+++ src/main/java/ru/vacancy/vacancyproject/VacancyProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.vacancy.vacancyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacancyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vacancy/vacancyproject/VacancyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vacancy.vacancyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacancyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
